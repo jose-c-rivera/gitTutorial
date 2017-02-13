61dd173c6f37d9c8f732643ecd2de63332fb8a89
Jose Rivera <jrivera4@uwo.ca>
2017-02-13 13:11:22 -0500

    Added Course classes and made changes to Student and Instructor

---
+++ src/Course.java
@@ -0,0 +1,12 @@
+/**
+ * Created by jose on 13/02/17.
+ */
+public class Course {
+    String cname;
+    String description;
+
+    public Course(String cname, String description){
+        this.cname = cname;
+        this.description = description;
+    }
+}
--- src/Student.java
+++ src/Student.java
@@ -1,8 +1,15 @@
+
+import java.util.List;
 /**
  * Created by ethan on 2017-02-07.
  */
 public class Student extends Person {
     String major;
+    List<Course> courseList;
+
+    public void addCourse(Course c){
+        courseList.add(c);
+    }
 
     public Student(String name, String emailAddress, String major) {
         super(name, emailAddress);
